011e4336749cd30f46411689aee4295dda2a8c0f
William R Peters <31421828+peterswi@users.noreply.github.com>
2020-10-30 19:30:44 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //IMPORT statements here
 import pie from './pie'
-import staticBar from './staticbar_script'
+import staticBar from './staticbar_erin'
 
 const staticPie=pie('.pie')
 const bar=staticBar('.erin-bar')
